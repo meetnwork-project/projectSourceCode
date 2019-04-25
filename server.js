e2a67513e079436fad6d23ed5e981bc7089f8368
elizrobinson <elizabeth.robinson@colorado.edu>
2019-04-25 12:47:07 -0600

    Liz's changes on homepage and server files

--- server.js
+++ server.js
@@ -1,121 +1,326 @@
-var express = require('express'); 
+var express = require('express'); //Ensure our express framework has been added
 var app = express();
-var bodyParser = require('body-parser'); 
-app.use(bodyParser.json());              
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+// const { body,validationResult } = require('express-validator/check');
+// const { sanitizeBody } = require('express-validator/filter');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var pgp = require('pg-promise')();
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'meetnwork_db',
+	user: 'postgres',
+	password: 'user'
+};
 
 var db = pgp(dbConfig);
+
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
 
 
-// app.post('/register',function(req,res)){//the post of register page
-// 	var user_name=req.body.user_name;
-// 	var user_email=req.body.user_email;
-// 	var user_password=req.body.user_password;
-// 	var user_first_name=req.body.user_first_name;
-// 	var user_last_name=req.body.user_last_name;
-// 	var major=req.body.M_choice;
-// 	var year=req.body.Y_choice;
-// 	var class=req.body.C_choice;
-// 	var login_info="INSERT INTO login_table(user_name, user_email, user_password)VALUES('" + user_name + "','" + user_email + "','" + user_password +"') ON CONFLICT DO NOTHING;";
-// 	var user_info="INSERT INTO user_table(first_name, last_name, major, year, classes, status)VALUES('" + user_first_name + "','" + user_last_name + "') ON CONFLICT DO NOTHING;";
-// 	// the var user_info is uncompleted because we lack of year, major ,and class
-
-// 	db.task('get-everything', task =>{
-// 		return task.batch([
-// 		task.any(login_info),
-// 		task.any(user_info)
-// 		]);
-// 	})
-//    .then(info =>{
-// 	   res.render('/register',{
-// 		   my_title: "register page",
-// 		   //uncompleted
-// 	   })
-//    })
-//    .catch(error =>{
-// 	   request.flash('error, err');
-// 	   response.render('/register',{
-// 		   my_title:'register page',
-// 		   //uncompleted
-// 	   })
-//    })
-// }
-
-// app.get('/logpage',function(req,res){
-// 	var user_id=req.body.user_name;
-// 	var user_password=req.body.user_password;
-// 	var login ="select * from login_table where user_name='"+user_id+"'";
-// 	db.any(login).then(function(/*what function we should use?*/){
-// 		res.render('/logpage',{
-// 			title:'log in page',
-// 			//uncompleted
-// 		})
-// 	})
-// 	.catch(function(err){
-// 		request.flash('error',err);
-// 		response.render('/logpage',{
-// 			title:'log in page',
-// 			//not complete
-// 		})
-// 	})
-// })
-
+// registration page 
+app.get('/register', function(req, res) {
+	res.render('pages/register',{
+		my_title:"Registration Page",
+		data: ''
+	})
+});
 
 // login page 
 app.get('/', function(req, res) {
 	res.render('pages/login',{
 		local_css:"signin.css", 
 		my_title:"Login Page"
-	});
-});
-// registration page 
-app.get('/register', function(req, res) {
-	res.render('pages/register',{
-		my_title:"Registration Page"
-	});
+	})
 });
 
 // profile page
 app.get('/profile', function(req, res) {
 	res.render('pages/profile',{
 		my_title:"Profile Page"
-	});
+	})
 });
 
 //edit profile page
 app.get('/profile_edit', function(req, res) {
 	res.render('pages/profile_edit',{
 		my_title:"Edit Profile Page"
-	});
+	})
 });
 
 // home page
 
 app.get('/home', function(req, res) {
-	var query = 'select classes from user_table ';
-	query += "WHERE first_name = 'Elizabeth';";
-	console.log(query);
+	res.cookie('name', 'elro2540@colorado.edu'); //Sets email, (move to login when working)
+	var current_user_email = req.cookies['name'];
 
-	db.any(query)
-    .then(function (rows) {
-    	console.log(rows)
-        res.render('pages/home',{
-			my_title: 'Home Page',
-			data: rows
-		})
+	var current_user_name = "SELECT user_table.first_name, user_table.last_name FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	current_user_name += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"';";
 
-    })
-    .catch(function (err) {
-        // display error message in case an error
-        request.flash('error', err);
-        response.render('pages/home', {
-            title: 'Home Page',
-            data: ''
-        })
-    });
+	var current_user_id = "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	current_user_id += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"';";
+
+	var all_classes = 'SELECT class_name FROM class_table;';
+
+	var user_todo_classes = 'SELECT classes FROM user_todos WHERE user_id = ANY(';
+	user_todo_classes += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	user_todo_classes += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+	var other_users = 'SELECT user_todos.user_id, todos, user_todos.classes , first_name, last_name, ';
+	other_users += 'user_todos.location, user_todos.location_desc ';
+	other_users += 'FROM user_todos INNER JOIN user_table ON (user_todos.user_id = user_table.user_id)';
+	other_users += 'WHERE user_todos.classes && ';
+	other_users += 'ANY(SELECT classes FROM user_todos WHERE user_id = ANY(';
+	other_users += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	other_users += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"'));";
 	
+	var all_user_classes = 'SELECT classes FROM user_table WHERE user_id = ANY(';
+	all_user_classes += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	all_user_classes += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+
+
+	db.task('get-everything', task =>{
+			return task.batch([
+			task.any(current_user_name),
+			task.any(current_user_id),
+			task.any(all_classes),
+			task.any(user_todo_classes),
+			task.any(other_users),
+			task.any(all_user_classes)
+			]);
+		})
+	.then(info =>{
+	   res.render('pages/home',{
+		   my_title: "Home Page",
+		   data: info[2],
+		   my_data: info[3],
+		   other_userdata: info[4],
+		   user_classes:info[5],
+		   curr_users_name:info[0],
+		   curr_users_id:info[1]
+	   })
+   })
+   .catch(error =>{
+	   request.flash('error, err');
+	   response.render('pages/home',{
+		   my_title:'Home Page',
+		   data : '',
+		   my_data: '',
+		   other_userdata: '',
+		   user_classes:'',
+		   curr_users_name:'',
+		   curr_users_id:''
+	   })
+   })
+});
+
+//On homepage, updates status, location, and 'how can I find you?'
+app.post('/home/status', function(req, res) {
+  var location = req.body.location;
+  var location_desc = req.body.location_desc;
+  var current_user_email = req.cookies['name'];
+
+  var insert_statement = "UPDATE user_todos SET location = '"+location+"' WHERE user_id = ";
+  insert_statement += "(SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+  insert_statement += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+  var insert_statement2 = "UPDATE user_todos SET location_desc = '"+location_desc+"' WHERE user_id = ";
+  insert_statement2 += "(SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+  insert_statement2 += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+  
+  db.task('get-everything', task => {
+        return task.batch([
+            task.any(insert_statement),
+            task.any(insert_statement2)
+        ]);
+    })
+    var current_user_name = "SELECT user_table.first_name, user_table.last_name FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	current_user_name += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"';";
+
+	var current_user_id = "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	current_user_id += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"';";
+
+	var all_classes = 'SELECT class_name FROM class_table;';
+
+	var user_todo_classes = 'SELECT classes FROM user_todos WHERE user_id = ANY(';
+	user_todo_classes += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	user_todo_classes += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+	var other_users = 'SELECT user_todos.user_id, todos, user_todos.classes , first_name, last_name, ';
+	other_users += 'user_todos.location, user_todos.location_desc ';
+	other_users += 'FROM user_todos INNER JOIN user_table ON (user_todos.user_id = user_table.user_id)';
+	other_users += 'WHERE user_todos.classes && ';
+	other_users += 'ANY(SELECT classes FROM user_todos WHERE user_id = ANY(';
+	other_users += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	other_users += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"'));";
+	
+	var all_user_classes = 'SELECT classes FROM user_table WHERE user_id = ANY(';
+	all_user_classes += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	all_user_classes += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+
+
+	db.task('get-everything', task =>{
+			return task.batch([
+			task.any(current_user_name),
+			task.any(current_user_id),
+			task.any(all_classes),
+			task.any(user_todo_classes),
+			task.any(other_users),
+			task.any(all_user_classes)
+			]);
+		})
+	.then(info =>{
+	   res.render('pages/home',{
+		   my_title: "Home Page",
+		   data: info[2],
+		   my_data: info[3],
+		   other_userdata: info[4],
+		   user_classes:info[5],
+		   curr_users_name:info[0],
+		   curr_users_id:info[1]
+	   })
+   })
+   .catch(error =>{
+	   request.flash('error, err');
+	   response.render('pages/home',{
+		   my_title:'Home Page',
+		   data : '',
+		   my_data: '',
+		   other_userdata: '',
+		   user_classes:'',
+		   curr_users_name:'',
+		   curr_users_id:''
+	   })
+   })
+});
+
+
+app.post('/home/todos', function(req, res) {//in an ideal world, this would be dynamic
+	var classes = [];
+	var todos =[];
+	if(req.body.class1!=undefined && req.body.class1!=''){
+		var class1 = req.body.class1;
+		classes.push(1);
+		todos.push(class1.toString());
+
+	}
+	if(req.body.class2!=undefined && req.body.class2!=''){
+		var class2 = req.body.class2;
+		classes.push(2);
+		todos.push(class2.toString());
+
+	}
+	if(req.body.class3!=undefined && req.body.class3!=''){
+		var class3 = req.body.class3;
+		todos.push(class3.toString());
+		classes.push(3);
+
+	}
+	if(req.body.class4!=undefined && req.body.class4!=''){
+		var class4 = req.body.class4;
+		classes.push(4);
+		todos.push(class4.toString());
+
+	}
+
+  // var insert_statement = 'INSERT INTO user_todos (todos) VALUES('+todos+')';
+  // insert_statement += ' WHERE user_id = 11;';
+
+  var current_user_email = req.cookies['name'];
+  var insert_statement = 'UPDATE user_todos SET todos = ARRAY[';
+  var length = todos.length;
+  var i = 1;
+  todos.forEach(function(each_todo){
+  		insert_statement += "'"+each_todo+"'";
+  		if(i != length){
+  			insert_statement += ', ';
+  		}
+  		i +=1;
+  	});
+  insert_statement += '] WHERE user_id = ';
+  insert_statement += "(SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+  insert_statement += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+  var insert_statement2 = "UPDATE user_todos SET classes = ARRAY["+classes+"] WHERE user_id = ";
+  insert_statement2 += "(SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+  insert_statement2 += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+  
+
+  console.log(insert_statement)
+  db.task('get-everything', task => {
+        return task.batch([
+            task.any(insert_statement),
+            task.any(insert_statement2)
+        ]);
+    })
+  var current_user_name = "SELECT user_table.first_name, user_table.last_name FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	current_user_name += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"';";
+
+	var current_user_id = "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	current_user_id += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"';";
+
+	var all_classes = 'SELECT class_name FROM class_table;';
+
+	var user_todo_classes = 'SELECT classes FROM user_todos WHERE user_id = ANY(';
+	user_todo_classes += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	user_todo_classes += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+	var other_users = 'SELECT user_todos.user_id, todos, user_todos.classes , first_name, last_name, ';
+	other_users += 'user_todos.location, user_todos.location_desc ';
+	other_users += 'FROM user_todos INNER JOIN user_table ON (user_todos.user_id = user_table.user_id)';
+	other_users += 'WHERE user_todos.classes && ';
+	other_users += 'ANY(SELECT classes FROM user_todos WHERE user_id = ANY(';
+	other_users += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	other_users += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"'));";
+	
+	var all_user_classes = 'SELECT classes FROM user_table WHERE user_id = ANY(';
+	all_user_classes += "SELECT user_table.user_id FROM user_table INNER JOIN login_table ON(user_table.user_name = ";
+	all_user_classes += "login_table.user_name) WHERE login_table.user_email = '"+current_user_email+"');";
+
+
+
+	db.task('get-everything', task =>{
+			return task.batch([
+			task.any(current_user_name),
+			task.any(current_user_id),
+			task.any(all_classes),
+			task.any(user_todo_classes),
+			task.any(other_users),
+			task.any(all_user_classes)
+			]);
+		})
+	.then(info =>{
+	   res.render('pages/home',{
+		   my_title: "Home Page",
+		   data: info[2],
+		   my_data: info[3],
+		   other_userdata: info[4],
+		   user_classes:info[5],
+		   curr_users_name:info[0],
+		   curr_users_id:info[1]
+	   })
+   })
+   .catch(error =>{
+	   request.flash('error, err');
+	   response.render('pages/home',{
+		   my_title:'Home Page',
+		   data : '',
+		   my_data: '',
+		   other_userdata: '',
+		   user_classes:'',
+		   curr_users_name:'',
+		   curr_users_id:''
+	   })
+   })
 });
 
 app.listen(3000);
